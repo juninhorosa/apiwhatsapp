573725b2a4519d7ba7b102e000f8a733c3f3cafc
juninhorosa <42284444+juninhorosa@users.noreply.github.com>
2026-04-13 21:32:52 +0100

    Fix: Corrigido erro de callback no Mongoose que impedia o registro de usuários

--- models/User.js
+++ models/User.js
@@ -17,13 +17,12 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+    if (!this.isModified('password')) return;
     this.password = await bcrypt.hash(this.password, 10);
     if (!this.apiKey) {
         this.apiKey = crypto.randomBytes(24).toString('hex');
     }
-    next();
 });
 
 // Compare password
